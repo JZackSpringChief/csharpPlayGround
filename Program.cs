8c9a3085e3e6f9a3c22fa7c8ad535620e3a5c4c9
Zack Spring Chief <jzacksc@gmail.com>
2023-10-23 11:50:58 -0600

    adding constructors

--- Program.cs
+++ Program.cs
@@ -2,17 +2,21 @@
 {
     class Program 
     {
-        class Unit {
-            public string? name;
+        class Unit 
+        {
+            public string name;
+            public int age;
             public void Attack() {
                 Console.WriteLine("Attacking!!!");
             }
         }  
-     public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Unit Hero = new();
-            // Hero.name = "Zack";
-            // Console.WriteLine(name);
+            Hero.name = "Zack";
+            Hero.age = 33;
+            Console.WriteLine(Hero.name);
+            Console.WriteLine(Hero.age);
             Hero.Attack();
         }  
     }
